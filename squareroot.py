d39a199b8a0cd1276478c6af25777d7ba3be3da8
g00387856 <60290531+g00387856@users.noreply.github.com>
2020-02-20 14:23:08 +0000

    Add files via upload

---
+++ squareroot.py
@@ -0,0 +1,15 @@
+#weekly task 6
+# a program taking a positive floating-point number 
+#as input and outputs an approximation of its square root
+
+
+import math
+
+number = float (input ("Enter a number to find the square root:"))
+sqrt = math.sqrt (number)
+#print ('the square root of {} is {} ',(number,sqrt))
+
+if number<0:
+    print ("Please enter valid number")
+else:
+    print ('the square root of {} is {} ',(number,sqrt))
