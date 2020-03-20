2c6dfa2a226554a8f4f8f15acd094fd27f7de6fb
unknown <g00387856@gmit.ie>
2020-03-20 08:39:16 +0000

    remove reverse ssentence

--- secondstring.py
+++ secondstring.py
@@ -12,12 +12,12 @@ sentence = (input ("Enter string :"))
 #reverse the sentence first
 
 
-sentence = sentence[::-1]
+#sentence = sentence[::-1]
 
 #print(sentence) uncomment this line to check the reverse if required
 
 #Now print every second letter of the reversed sentence
 
-print ("This is my sentence with every second letter in reverse order :",sentence[::2])
+print ("This is my sentence with every second letter in reverse order :",sentence[::-2])
 
  
